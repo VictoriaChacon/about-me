2c041d6c8695e95c82dc604569995dc7ec4301d1
Victoria Chacon <Vchaco06@gmail.com>
2016-07-13 08:23:44 -0600

    added some paragraphs and a picture

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<!--linked to CSS stylesheet-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<title>Victoria Chacon</title>
+	</head>
+	<body>
+		<!--begin header here-->
+		<header>
+			<h1>This is my About Me Page</h1>
+		</header>
+		<!--Start the main content here-->
+		<main>
+			<h2>A little bit about me!</h2>
+			<img src="images/victoria1.jpg"/>
+			<p> Hello! My name is Victoria Chacon. <br> I am a current student of CNM's Deep Dive Coding Bootcamp. Some previous scholl history includes a Bachelor's degree from the University of New Mexico in Profession Writing. I have used my writing skills to intern for a local magazine ABQ-live. I also went through CNM's Digital Media track and gained experience in graphic design. I used these skills both at ABQ-live and a non-profit organization known as Alta Mira Specialized Family Services.</p>
+
+		</main>
+
+	</body>
+</html>
